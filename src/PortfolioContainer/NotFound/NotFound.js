ab77c8f1a3a1bd30856ade78a7754c7a9ca33ee2
kinya-lindah <32284484+kinya-lindah@users.noreply.github.com>
2022-01-14 11:50:35 -0800

    Add files via upload

---
+++ src/PortfolioContainer/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link as LinkRouter } from 'react-router-dom';
+import { color1 } from '../../AppVariables';
+
+export default function NotFound() {
+    return (
+        <div style={{display:'flex', flexDirection:'column', height:'100vh', paddingTop: '100px'}}>
+             <h1 style={{color:color1}}> 404 Page Not Found</h1>
+            <LinkRouter to="/"> Return to home page</LinkRouter>
+        </div>
+    )
+}
