0059fb523a32dac766c8aec75a38005e0efe091a
kinya-lindah <32284484+kinya-lindah@users.noreply.github.com>
2022-01-14 10:14:00 -0800

    Add files via upload

---
+++ src/PortfolioContainer/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Profile from './Profile/Profile';
+
+import CurlyLine from './CurlyLine/CurlyLine';
+
+export default function Home() {
+    
+    return (
+        <div id='top'>
+            <Profile />
+            <CurlyLine/>
+            
+        </div>
+    )
+}
