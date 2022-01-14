3b2cd5e465dcab99eaae7241e295a4786009e201
Lindah Kinya <lindahkinya@Lindahs-MacBook-Air.local>
2022-01-14 11:54:18 -0800

    Updates

---
+++ precache-manifest.adb847265be485e906d32f4d52ee7bae.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7d74f2f718b9d839d4c909a5ed6c2bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7799523c2dd1d3ce77b1",
+    "url": "/static/css/main.697ed1d7.chunk.css"
+  },
+  {
+    "revision": "e52988d3f5546b01c4af",
+    "url": "/static/js/2.6a4cfd18.chunk.js"
+  },
+  {
+    "revision": "302e4612d0bc7567a51d301135ed2f12",
+    "url": "/static/js/2.6a4cfd18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "300a2cba2c6624858100",
+    "url": "/static/js/3.566cf62a.chunk.js"
+  },
+  {
+    "revision": "7799523c2dd1d3ce77b1",
+    "url": "/static/js/main.944d5885.chunk.js"
+  },
+  {
+    "revision": "af0bef69022e8c825daa",
+    "url": "/static/js/runtime-main.9e13db43.js"
+  },
+  {
+    "revision": "f12ccd6336bf5f1b6b5082fb04b5da62",
+    "url": "/static/media/LoginNB.f12ccd63.jpeg"
+  },
+  {
+    "revision": "c34d3848df8076885a573938738c5b5f",
+    "url": "/static/media/MN1NB.c34d3848.jpeg"
+  },
+  {
+    "revision": "bd7130adf601557a546c0d516f50b63e",
+    "url": "/static/media/NB2Open.bd7130ad.jpeg"
+  },
+  {
+    "revision": "6f30053cd73f88af32b6585095b1e601",
+    "url": "/static/media/PI1NB.6f30053c.jpeg"
+  },
+  {
+    "revision": "044dfdf57a08a344560e8f2bd31a50dd",
+    "url": "/static/media/computerMessage.044dfdf5.png"
+  },
+  {
+    "revision": "5b1b6f62320058d08373ab1d23ebbfc1",
+    "url": "/static/media/drawMessage.5b1b6f62.png"
+  },
+  {
+    "revision": "56467289a70662d624c92880df60f1f3",
+    "url": "/static/media/emptyScreen.56467289.png"
+  },
+  {
+    "revision": "08e41bfcfcc63974dc3a6551a421a89e",
+    "url": "/static/media/homeNB.08e41bfc.jpeg"
+  },
+  {
+    "revision": "4106369dacbd736054d6d31471fe17f9",
+    "url": "/static/media/lindahMessage.4106369d.png"
+  },
+  {
+    "revision": "d8428519194ad09320d761bdfdacf9c5",
+    "url": "/static/media/matrix2.d8428519.mp4"
+  },
+  {
+    "revision": "51dbe3a04d77fc09b3113e8e7aefbb0e",
+    "url": "/static/media/me.51dbe3a0.png"
+  },
+  {
+    "revision": "6672b4d185d53c7cf4cd34e967981af0",
+    "url": "/static/media/me.6672b4d1.jpeg"
+  },
+  {
+    "revision": "0b84cb2bbafb64d92d54fcb12c1ae259",
+    "url": "/static/media/ongoingGame.0b84cb2b.png"
+  },
+  {
+    "revision": "6a7f7491af00e7a53e2127939fb75a50",
+    "url": "/static/media/scoresScreen.6a7f7491.png"
+  },
+  {
+    "revision": "b928f892d6233a5286b6e8a8d545e179",
+    "url": "/static/media/serious.b928f892.jpeg"
+  },
+  {
+    "revision": "0b01df53dfb09f87eb80d1d3f776549e",
+    "url": "/static/media/serious2.0b01df53.jpeg"
+  },
+  {
+    "revision": "3e5811d00fd125be55af751f00ad12fe",
+    "url": "/static/media/texturedGrey.3e5811d0.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reactPortfolio/precache-manifest.22b6d7be1aa196a32aca4b84f00bdb10.js"
+  "/precache-manifest.adb847265be485e906d32f4d52ee7bae.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactPortfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
