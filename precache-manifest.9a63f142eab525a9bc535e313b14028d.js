684057c7e1f3bfd09ef637e218f4ee19d6aa627c
Lindah Kinya <lindahkinya@Lindahs-MacBook-Air.local>
2022-01-14 10:53:32 -0800

    Updates

---
+++ precache-manifest.9a63f142eab525a9bc535e313b14028d.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa069ad3f2bbbc4d09e3e84002248fa6",
+    "url": "/reactPortfolio/index.html"
+  },
+  {
+    "revision": "e21cb36660d6027b1b47",
+    "url": "/reactPortfolio/static/css/main.2eb34c83.chunk.css"
+  },
+  {
+    "revision": "0f7954d498f509eeea78",
+    "url": "/reactPortfolio/static/js/2.426cd69d.chunk.js"
+  },
+  {
+    "revision": "302e4612d0bc7567a51d301135ed2f12",
+    "url": "/reactPortfolio/static/js/2.426cd69d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "300a2cba2c6624858100",
+    "url": "/reactPortfolio/static/js/3.566cf62a.chunk.js"
+  },
+  {
+    "revision": "e21cb36660d6027b1b47",
+    "url": "/reactPortfolio/static/js/main.1bc01bc8.chunk.js"
+  },
+  {
+    "revision": "ab99ea61f5d77b86c1fe",
+    "url": "/reactPortfolio/static/js/runtime-main.92e50aa8.js"
+  },
+  {
+    "revision": "f12ccd6336bf5f1b6b5082fb04b5da62",
+    "url": "/reactPortfolio/static/media/LoginNB.f12ccd63.jpeg"
+  },
+  {
+    "revision": "c34d3848df8076885a573938738c5b5f",
+    "url": "/reactPortfolio/static/media/MN1NB.c34d3848.jpeg"
+  },
+  {
+    "revision": "bd7130adf601557a546c0d516f50b63e",
+    "url": "/reactPortfolio/static/media/NB2Open.bd7130ad.jpeg"
+  },
+  {
+    "revision": "6f30053cd73f88af32b6585095b1e601",
+    "url": "/reactPortfolio/static/media/PI1NB.6f30053c.jpeg"
+  },
+  {
+    "revision": "044dfdf57a08a344560e8f2bd31a50dd",
+    "url": "/reactPortfolio/static/media/computerMessage.044dfdf5.png"
+  },
+  {
+    "revision": "5b1b6f62320058d08373ab1d23ebbfc1",
+    "url": "/reactPortfolio/static/media/drawMessage.5b1b6f62.png"
+  },
+  {
+    "revision": "56467289a70662d624c92880df60f1f3",
+    "url": "/reactPortfolio/static/media/emptyScreen.56467289.png"
+  },
+  {
+    "revision": "08e41bfcfcc63974dc3a6551a421a89e",
+    "url": "/reactPortfolio/static/media/homeNB.08e41bfc.jpeg"
+  },
+  {
+    "revision": "4106369dacbd736054d6d31471fe17f9",
+    "url": "/reactPortfolio/static/media/lindahMessage.4106369d.png"
+  },
+  {
+    "revision": "d8428519194ad09320d761bdfdacf9c5",
+    "url": "/reactPortfolio/static/media/matrix2.d8428519.mp4"
+  },
+  {
+    "revision": "51dbe3a04d77fc09b3113e8e7aefbb0e",
+    "url": "/reactPortfolio/static/media/me.51dbe3a0.png"
+  },
+  {
+    "revision": "6672b4d185d53c7cf4cd34e967981af0",
+    "url": "/reactPortfolio/static/media/me.6672b4d1.jpeg"
+  },
+  {
+    "revision": "0b84cb2bbafb64d92d54fcb12c1ae259",
+    "url": "/reactPortfolio/static/media/ongoingGame.0b84cb2b.png"
+  },
+  {
+    "revision": "6a7f7491af00e7a53e2127939fb75a50",
+    "url": "/reactPortfolio/static/media/scoresScreen.6a7f7491.png"
+  },
+  {
+    "revision": "b928f892d6233a5286b6e8a8d545e179",
+    "url": "/reactPortfolio/static/media/serious.b928f892.jpeg"
+  },
+  {
+    "revision": "0b01df53dfb09f87eb80d1d3f776549e",
+    "url": "/reactPortfolio/static/media/serious2.0b01df53.jpeg"
+  },
+  {
+    "revision": "3e5811d00fd125be55af751f00ad12fe",
+    "url": "/reactPortfolio/static/media/texturedGrey.3e5811d0.png"
+  }
+]);
