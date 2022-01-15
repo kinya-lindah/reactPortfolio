2da8e423cb596005ff43ce38db55f5c3672e0b4f
kinya-lindah <32284484+kinya-lindah@users.noreply.github.com>
2022-01-14 16:29:58 -0800

    Add files via upload

---
+++ src/Pages/NotFoundPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import NotFound from '../PortfolioContainer/NotFound/NotFound'
+
+export default function NotFoundPage() {
+    return (
+        <div>
+            <NotFound/>
+        </div>
+    )
+};
